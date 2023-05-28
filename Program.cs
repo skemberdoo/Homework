60076b2d2d107e5926afbad00bd8e7381236d96a
Mark Alexson Abrina <markalexsonabrina@Marks-MacBook-Air.local>
2023-05-28 10:13:58 +0800

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//System for calculating Angles between hours and minutes
+
+using System;
+
+namespace ClockAngleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get user input for hours and minutes
+            Console.Write("Enter the hour (0-12): ");
+            int hour = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the minutes (0-59): ");
+            int minutes = Convert.ToInt32(Console.ReadLine());
+
+            // Calculate the smaller angle between the hour and minute hands
+            double hourAngle = 0.5 * (60 * hour + minutes);
+            double minuteAngle = 6 * minutes;
+            double angle = Math.Abs(hourAngle - minuteAngle);
+
+            // Ensure the smaller angle is considered
+            if (angle > 180)
+                angle = 360 - angle;
+
+            // Display the result
+            Console.WriteLine($"The smaller angle between the hour and minute hands is: {angle} degrees");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ part2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Branch
+{
+    public string Name { get; set; }
+    public List<Branch> Branches { get; set; }
+
+    public Branch(string name)
+    {
+        Name = name;
+        Branches = new List<Branch>();
+    }
+}
+
+public class Program
+{
+    public static int CalculateDepth(Branch branch)
+    {
+        if (branch.Branches.Count == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            int maxDepth = 0;
+            foreach (var child in branch.Branches)
+            {
+                int childDepth = CalculateDepth(child);
+                if (childDepth > maxDepth)
+                {
+                    maxDepth = childDepth;
+                }
+            }
+            return maxDepth + 1;
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        
+        Branch branch5 = new Branch("Branch 5");
+        Branch branch4 = new Branch("Branch 4");
+        branch4.Branches.Add(branch5);
+        Branch branch3 = new Branch("Branch 3");
+        branch3.Branches.Add(branch4);
+        Branch branch2 = new Branch("Branch 2");
+        branch2.Branches.Add(branch3);
+        Branch branch1 = new Branch("Branch 1");
+        branch1.Branches.Add(branch2);
+
+        int depth = CalculateDepth(branch1);
+        Console.WriteLine("Depth: " + depth);
+    }
+}
